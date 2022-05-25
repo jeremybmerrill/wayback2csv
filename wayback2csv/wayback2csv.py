ad4c75db85f2c84ce15e0bcaae75890c80d68352
Jeremy Merrill <jeremy.merrill@washpost.com>
2022-05-25 15:57:25 -0400

    autoformat

--- wayback2csv/wayback2csv.py
+++ wayback2csv/wayback2csv.py
@@ -4,15 +4,19 @@ from datetime import datetime
 import csv
 import json
 import logging
+
+import requests
+import waybackpack
+from bs4 import BeautifulSoup
+from tqdm.auto import tqdm
+
 logger = logging.getLogger(__name__)
 
-from tqdm.auto import tqdm
-from bs4 import BeautifulSoup
-import waybackpack
-import requests
 
 DEFAULT_PARENT_DIR = ".wayback2csv/"
-DEFAULT_COLLAPSE = "timestamp:8" # daily
+DEFAULT_COLLAPSE = "timestamp:8"  # daily
+
+
 class Wayback2Csv:
 
     def __init__(self, user_agent, url, from_date=None, to_date=None, collapse=DEFAULT_COLLAPSE, dir=DEFAULT_PARENT_DIR):
@@ -20,14 +24,14 @@ class Wayback2Csv:
         self.dir = dir
 
         snapshots = waybackpack.search(url,
-            session=session,
-            from_date=from_date,
-            to_date=to_date,
-            uniques_only=True,
-            collapse=collapse
-        )
+                                       session=session,
+                                       from_date=from_date,
+                                       to_date=to_date,
+                                       uniques_only=True,
+                                       collapse=collapse
+                                       )
 
-        timestamps = [ snap["timestamp"] for snap in snapshots ]
+        timestamps = [snap["timestamp"] for snap in snapshots]
 
         self.pack = waybackpack.Pack(
             url,
@@ -56,7 +60,7 @@ class Wayback2Csv:
         filepath = os.path.join(filedir, path_tail)
         logger.info(
             "Fetching {0} @ {1}".format(
-                self.pack.url, 
+                self.pack.url,
                 timestamp)
         )
 
@@ -64,7 +68,7 @@ class Wayback2Csv:
             content = requests.get('https://' + self.pack.url).content
         except Exception as e:
             if ignore_errors == True:
-                ex_name = ".".join([ e.__module__, e.__class__.__name__ ])
+                ex_name = ".".join([e.__module__, e.__class__.__name__])
                 logger.warn("ERROR -- {0} @ {1} -- {2}: {3}".format(
                     self.pack.url,
                     timestamp,
@@ -86,64 +90,68 @@ class Wayback2Csv:
         path_head, path_tail = os.path.split(self.pack.parsed_url.path)
         self.values = []
         for fn in tqdm(glob(os.path.join(
-            self.dir, 
-            "*", 
+            self.dir,
+            "*",
             self.pack.parsed_url.netloc,
             path_head.lstrip("/"),
             path_tail
-            ))):
-                if os.path.isdir(fn): continue
-                with open(fn, 'r') as f:
-                    try:
-                        html_doc = f.read()
-                    except UnicodeDecodeError:
-                        continue
-                    soup = BeautifulSoup(html_doc, 'html.parser')
-                    followers_values = soup.select(css_selector)
-                    if not followers_values:
-                        print("no followers_value", fn)
-                        continue
-                    followers = number_lambda(followers_values[0]) if number_lambda else followers_values[0]
-                    try:
-                        count = int(followers)
-                    except ValueError as e:
-                        print("int problems", fn)
-                        continue
-                    raw_date = fn.split("/")[1]
-                    scrape_date = datetime.strptime(raw_date[:8], "%Y%m%d")
-                    self.values.append([fn, scrape_date, count])
-    
+        ))):
+            if os.path.isdir(fn):
+                continue
+            with open(fn, 'r') as f:
+                try:
+                    html_doc = f.read()
+                except UnicodeDecodeError:
+                    continue
+                soup = BeautifulSoup(html_doc, 'html.parser')
+                followers_values = soup.select(css_selector)
+                if not followers_values:
+                    print("no followers_value", fn)
+                    continue
+                followers = number_lambda(
+                    followers_values[0]) if number_lambda else followers_values[0]
+                try:
+                    count = int(followers)
+                except ValueError as e:
+                    print("int problems", fn, e)
+                    continue
+                raw_date = fn.split("/")[1]
+                scrape_date = datetime.strptime(raw_date[:8], "%Y%m%d")
+                self.values.append([fn, scrape_date, count])
+
     def parse_json(self, path, number_lambda=None):
         path_head, path_tail = os.path.split(self.pack.parsed_url.path)
         self.values = []
         for fn in tqdm(glob(os.path.join(
-            self.dir, 
-            "*", 
+            self.dir,
+            "*",
             self.pack.parsed_url.netloc,
             path_head.lstrip("/"),
             path_tail
-            ))):
-                if os.path.isdir(fn): continue
-                with open(fn, 'r') as f:
-                    try:
-                        json_doc = f.read()
-                    except UnicodeDecodeError:
-                        continue
-                    try:
-                        data = json.loads(json_doc)
-                    except json.decoder.JSONDecodeError:
-                        print("JSON decode error {}".format(fn))
-                        continue            
-                    count = drill_down_nested_dict(data, path)
-                    count = number_lambda(count) if number_lambda else count
-                    raw_date = fn.split("/")[1]
-                    scrape_date = datetime.strptime(raw_date[:8], "%Y%m%d")
-                    self.values.append([fn, scrape_date, count])
+        ))):
+            if os.path.isdir(fn):
+                continue
+            with open(fn, 'r') as f:
+                try:
+                    json_doc = f.read()
+                except UnicodeDecodeError:
+                    continue
+                try:
+                    data = json.loads(json_doc)
+                except json.decoder.JSONDecodeError:
+                    print("JSON decode error {}".format(fn))
+                    continue
+                count = drill_down_nested_dict(data, path)
+                count = number_lambda(count) if number_lambda else count
+                raw_date = fn.split("/")[1]
+                scrape_date = datetime.strptime(raw_date[:8], "%Y%m%d")
+                self.values.append([fn, scrape_date, count])
 
     def to_csv(self, outfn, extra_row_values=[]):
         with open(outfn, 'w') as outf:
             writer = csv.writer(outf)
-            writer.writerow(["path", "date", "value"] + [f"extra{n+1}" for n,_ in enumerate(extra_row_values)])
+            writer.writerow(["path", "date", "value"] +
+                            [f"extra{n+1}" for n, _ in enumerate(extra_row_values)])
             for row in self.values:
                 writer.writerow(row + extra_row_values)
 
